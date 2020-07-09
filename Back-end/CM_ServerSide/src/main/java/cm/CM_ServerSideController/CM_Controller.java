fa366d579fa7d8766033f510e8b152e4ddcbc7e7
Akhil Jobi <jobixolo@gmail.com>
2020-07-09 14:09:15 +0530

    test run

--- Back-end/CM_ServerSide/src/main/java/cm/CM_ServerSideController/CM_Controller.java
+++ Back-end/CM_ServerSide/src/main/java/cm/CM_ServerSideController/CM_Controller.java
@@ -1,5 +1,42 @@
 package cm.CM_ServerSideController;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cm.CM_ServerSideModel.CM_Model;
+import cm.CM_ServerSideService.CM_Service;
+
+
+@RestController
+@RequestMapping("")
 public class CM_Controller {
 
+	@Autowired
+	private CM_Service cmService;
+	
+	@GetMapping("/commodities")
+	public List<CM_Model> getAllCommodity(){
+		System.out.println("jhg");
+		return cmService.getAll();
+	}
+	
+	@GetMapping("/commodities/{xyz}")
+	public List<CM_Model> searchCommodity(@PathVariable String xyz){
+		return cmService.search(xyz);
+	}
+	
+	@PostMapping("/commodities")
+	public String addCommodity(@Valid @RequestBody CM_Model c) {
+		return cmService.addCommodity(c);
+	}
+	
 }
